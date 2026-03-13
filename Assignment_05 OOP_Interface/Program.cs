5c317b5b4b63752ad2fc18e98ffe137183154228
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-03-13 06:38:48 +0200

    Q_01

--- Assignment_05 OOP_Interface/Program.cs
+++ Assignment_05 OOP_Interface/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Q1
+            //  Q1 : What is an interface in C#? Why do we use interfaces instead of depending on concrete classes directly?
+            //  Mention at least three benefits of using interfaces.
+
+            /*
+             An interface in C# is a reference type that defines a contract.
+             It contains method signatures, properties, events, or indexers without implementation.
+             Any class that implements the interface must provide the implementation for all its members.
+
+
+            We use interfaces instead of depending directly on concrete classes
+            to achieve loose coupling and make the code more flexible and maintainable.
+             */
+            #endregion
         }
     }
 }
