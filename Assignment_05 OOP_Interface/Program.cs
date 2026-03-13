975a6d3c8406c056067905c60e4e8b884d10dab7
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-03-13 06:56:14 +0200

    Q_02

--- Assignment_05 OOP_Interface/Program.cs
+++ Assignment_05 OOP_Interface/Program.cs
@@ -18,6 +18,47 @@
             to achieve loose coupling and make the code more flexible and maintainable.
              */
             #endregion
+
+            #region Q2
+            //Q2: Look at the following code and answer the questions below:
+
+
+            //a) What is the problem with this design? Both interfaces have a method called Greet() —
+            //how does the class handle it currently?
+
+            /*
+             The problem is that both interfaces contain a method with the same name Greet().
+            This means that both IEnglishSpeaker and IArabicSpeaker will use the same method implementation
+             */
+
+            // b) How would you fix this so IEnglishSpeaker.Greet() says "Hello" and IArabicSpeaker.Greet() says "Ahlan" ?
+            //What is this technique called?
+            /*
+             must makw
+            public void IEnglishSpeaker.Greet()
+            {
+
+            } ;
+            public void IArabicSpeaker.Greet()
+            {
+
+            } ;
+            
+             */
+
+            //c) After applying your fix, can you call Greet() directly on a Translator object (e.g. translator.Greet())?
+            //Why or why not? How do you call each version?
+
+            /*
+             NO,I can't call Greet() directly .
+
+            ((IEnglishSpeaker)Translator).Greet() ;
+
+            ((IArabicSpeaker)Translator).Greet() ;
+             */
+
+
+            #endregion
         }
     }
 }
